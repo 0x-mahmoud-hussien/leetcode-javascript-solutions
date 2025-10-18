29bee55b48ed03914059545b1fbc06220cc18bbc
Mahmoud Hussien <mahmoud.abdelmaqsoud.hussien@gmail.com>
2025-10-19 02:32:41 +0300

    Add solution for Find the Duplicate Number

---
+++ FindtheDuplicateNumber.js
@@ -0,0 +1,19 @@
+
+// https://leetcode.com/problems/find-the-duplicate-number
+var findDuplicate = function(nums){
+
+    let seen = new Set();
+    
+    for(let num of nums){
+        if(seen.has(num)){
+            return num;
+        }
+        seen.add(num);
+    }
+
+    return -1;
+}
+
+console.log(findDuplicate([1,3,4,2,2]));
+console.log(findDuplicate([3,1,3,4,2]));
+console.log(findDuplicate([3,3,3,3,3]));
