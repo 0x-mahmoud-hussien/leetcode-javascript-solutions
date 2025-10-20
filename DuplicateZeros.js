73a537a09c6678d0b435585f7173d4c93cbc58fc
Mahmoud Hussien <mahmoud.abdelmaqsoud.hussien@gmail.com>
2025-10-21 02:01:56 +0300

    Add solution for Duplicate Zeros

---
+++ DuplicateZeros.js
@@ -0,0 +1,17 @@
+
+// https://leetcode.com/problems/duplicate-zeros
+var duplicateZeros = function(arr){
+
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === 0){
+            arr.splice(i, 0, 0);
+            i++;
+            arr.pop();
+        }
+    }
+
+    return arr;
+};
+
+console.log(duplicateZeros([1,0,2,3,0,4,5,0]));
+console.log(duplicateZeros([1,2,3]));
