3f41dd56faaf4b5a0ea6038ab53d82006369a7da
Mahmoud Hussien <mahmoud.abdelmaqsoud.hussien@gmail.com>
2025-10-21 01:42:39 +0300

    Update ExcelSheetColumnNumber.js

--- ExcelSheetColumnNumber.js
+++ ExcelSheetColumnNumber.js
@@ -1,15 +1,18 @@
 
 // https://leetcode.com/problems/excel-sheet-column-number
-var titleToNumber = function(columnTitle){
+// var titleToNumber = function(columnTitle){
 
-    let result = 0;
-    for (let char of columnTitle) {
-        result = result * 26 + (char.charCodeAt(0) - 64);
-    }
-    return result;
+//     let result = 0;
+//     for (let char of columnTitle) {
+//         result = result * 26 + (char.charCodeAt(0) - 64);
+//     }
+//     return result;
     
-};
+// };
 
-console.log(titleToNumber('A'));
-console.log(titleToNumber('AB'));
-console.log(titleToNumber('ZY'));
+// console.log(titleToNumber('A'));
+// console.log(titleToNumber('AB'));
+// console.log(titleToNumber('ZY'));
+
+
+
