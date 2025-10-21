bc49ff3831da482dafbdfb9a05f3f63608ad28c9
Mahmoud Hussien <mahmoud.abdelmaqsoud.hussien@gmail.com>
2025-10-22 00:28:04 +0300

    Add solution for Reverse String

---
+++ ReverseString.js
@@ -0,0 +1,9 @@
+
+// https://leetcode.com/problems/reverse-string
+var reverseString = function(s){
+
+    return s.reverse();
+};
+
+console.log(reverseString(["h","e","l","l","o"]));
+console.log(reverseString(["H","a","n","n","a","h"]));
