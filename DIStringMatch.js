68d1c098a7027b3755681479eaacad1c65b27bfc
Mahmoud Hussien <mahmoud.abdelmaqsoud.hussien@gmail.com>
2025-10-29 01:51:51 +0300

    Add solution for DI String Match

---
+++ DIStringMatch.js
@@ -0,0 +1,24 @@
+
+// https://leetcode.com/problems/di-string-match
+var diStringMatch = function(s) {
+    let result = [];
+    let low = 0;             
+    let high = s.length;      
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === 'I') {
+            result.push(low);  
+            low++;            
+        } else { // 'D'
+            result.push(high);
+            high--;            
+        }
+    }
+
+    result.push(low); 
+    return result;
+};
+
+console.log(diStringMatch("IDID"));
+console.log(diStringMatch("III"));
+console.log(diStringMatch("DDI"));
