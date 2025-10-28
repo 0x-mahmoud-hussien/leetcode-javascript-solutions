edad15faeaf941834aa44d7725176135561d606a
Mahmoud Hussien <mahmoud.abdelmaqsoud.hussien@gmail.com>
2025-10-29 00:23:40 +0300

    Add solution for Pascal's Triangle

---
+++ Pascal'sTriangle.js
@@ -0,0 +1,23 @@
+
+// https://leetcode.com/problems/pascals-triangle
+function generate(numRows) {
+    const result = [];
+
+    for (let i = 0; i < numRows; i++) {
+        const row = [1];
+
+        for (let j = 1; j < i; j++) {
+            row.push(result[i - 1][j - 1] + result[i - 1][j]);
+        }
+
+        if (i > 0) row.push(1);
+        result.push(row);
+    }
+
+    return result;
+}
+
+console.log(generate(5));
+console.log(generate(1));
+
+
