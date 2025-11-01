06d5faa0cb0e45cb3763c557007c1c3730f71531
Mahmoud Hussien <mahmoud.abdelmaqsoud.hussien@gmail.com>
2025-11-02 00:37:43 +0200

    Add solution for Merge Intervals

---
+++ MergeIntervals.js
@@ -0,0 +1,25 @@
+
+// https://leetcode.com/problems/merge-intervals?envType=problem-list-v2&envId=sorting
+var merge = function(intervals){
+
+   intervals.sort((a, b) => a[0] - b[0]);
+
+    let merged = [];
+
+    for (let interval of intervals) {
+        if (merged.length === 0 || merged[merged.length - 1][1] < interval[0]) {
+            merged.push(interval);
+        } else {
+            merged[merged.length - 1][1] = Math.max(
+                merged[merged.length - 1][1], 
+                interval[1]        
+            );
+        }
+    }
+
+    return merged;
+};
+
+console.log(merge([[1,3],[2,6],[8,10],[15,18]]));
+console.log(merge([[1,4],[4,5]]));
+console.log(merge([[4,7],[1,4]]));
