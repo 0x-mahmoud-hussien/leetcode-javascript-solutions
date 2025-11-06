31b3e310cb6a5846a7ce997ecae653b39b4688f6
Mahmoud Hussien <mahmoud.abdelmaqsoud.hussien@gmail.com>
2025-11-06 14:39:23 +0200

    Add solution for Longest Substring Without Repeating Characters

---
+++ LongestSubstringWithoutRepeatingCharacters.js
@@ -0,0 +1,20 @@
+
+// https://leetcode.com/problems/longest-substring-without-repeating-characters?envType=problem-list-v2&envId=hash-table
+var lengthOfLongestSubstring = function(s) {
+    let seen = new Set();
+    let left = 0;
+    let maxLength = 0;
+
+    for(let right = 0; right < s.legnth; right++) {
+        while(seen.has(s[right])) {
+            seen.delete(s[left]);
+            left++;
+        }
+        seen.add(s[right]);
+        maxLength = Math.max(maxLength, right - left + 1);
+    }
+
+    return maxLength;
+}
+
+console.log(lengthOfLongestSubstring("abcabcbb"));
