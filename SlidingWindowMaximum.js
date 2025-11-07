7630d9314f95b54fbe637b796ebdace9848577e7
Mahmoud Hussien <mahmoud.abdelmaqsoud.hussien@gmail.com>
2025-11-07 17:01:21 +0200

    Add solution for Sliding Window Maximum

---
+++ SlidingWindowMaximum.js
@@ -0,0 +1,27 @@
+
+// https://leetcode.com/problems/sliding-window-maximumذ
+var maxSlidingWindow = function(nums, k) {
+    let result = [];
+    let deque = [];
+
+    for(let i = 0; i < nums.length; i++) {
+        if(deque.length && deque[0] === i - k) {
+            deque.shift();
+        }
+
+        while(deque.length && nums[deque[deque.length - 1]] < nums[i]) {
+            deque.pop();
+        }
+
+        deque.push(i);
+
+        if(i >= k - 1) {
+            result.push(nums[deque[0]]);
+        }
+    }
+
+    return result;
+}
+
+console.log(maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3));
+console.log(maxSlidingWindow([1], 1));
