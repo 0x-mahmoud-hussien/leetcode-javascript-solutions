5718985400903681cec7639bfc8178113f1452f7
Mahmoud Hussien <mahmoud.abdelmaqsoud.hussien@gmail.com>
2025-11-07 10:59:54 +0200

    Update Find the Index of the First Occurrence in a String

--- FindtheIndexoftheFirstOccurrenceinaString.js
+++ FindtheIndexoftheFirstOccurrenceinaString.js
@@ -1,5 +1,5 @@
 
-
+// https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string
 var strStr = function(haystack, needle) {
     let m = haystack.length;
     let n = needle.length;
